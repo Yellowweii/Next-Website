6512e47e9f53da93aad21c476da810adf9ae6a85
yellowwei <1119765689hw@gmail.com>
2023-10-10 23:59:51 +0800

    finish some function of the hero module

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
     <section>
-      <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Yellowwei</h1>
-      <p className="text-[#adb7be]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">Hello, I'm Yellowwei</h1>
+          <p className="text-[#adb7be] mb-6 lg:text-xl text-base sm:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire Me</button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">Download CV</button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] flex items-center justify-center">
+            <Image className="absolute" src="/images/hero-image.png" alt="hero image" width={300} height={300} />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
