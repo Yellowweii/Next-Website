04feb3bd88424d132b3679922985dd59c6594462
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-10-10 20:58:11 +0800

    little update (#1)
    Co-authored-by: yellowwei <1119765689@qq.com>

---
+++ components/HeroSection/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Yellowwei</h1>
+      <p className="text-[#adb7be]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+    </section>
+  );
+};
+
+export default HeroSection;
