7a4a7ea49acb0c245ff2b6aa808a5752dea75ceb
yellowwei <1119765689hw@gmail.com>
2023-10-11 11:31:45 +0800

    stash

--- components/AboutSection/index.tsx
+++ components/AboutSection/index.tsx
@@ -49,7 +49,7 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white">
+    <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/about-image.png" alt="about image" width={500} height={500} />
         <div className="h-full">
