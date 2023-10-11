4e56f55a6809f5468558e654862160bc8fd2ab40
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-10-11 09:39:36 +0800

    finish the navbar module (#4)
    * finish some function of the hero module
    
    * finish the type words effect
    
    * finish the navbar module

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -5,7 +5,7 @@ import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
-    <section>
+    <section className="py-20 lg:py-16">
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
---
+++ components/NavLink/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Link from "next/link";
+
+interface NavLinkProps {
+  title: string;
+  href: string;
+}
+
+const NavLink = ({ title, href }: NavLinkProps) => {
+  return (
+    <div>
+      <Link className="block py-2 pl-3 pr-4 text-[#adb7be] sm:text-xl rounded md:p-0 hover:text-white" href={href}>
+        {title}
+      </Link>
+    </div>
+  );
+};
+
+export default NavLink;
