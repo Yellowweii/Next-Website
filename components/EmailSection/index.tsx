09a92cc11f1f2f72c848d8ee410c417954709509
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-10-12 10:51:49 +0800

    little change (#10)
    * finish some function of the hero module
    
    * finish the type words effect
    
    * finish the navbar module
    
    * finish the about section
    
    * stash
    
    * complete some function of the project module
    
    * complete some function of the projectTag module
    
    * finish the ProjectSection module

---
+++ components/EmailSection/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import GithubICon from "../../public/github-icon.svg";
+import LinkedInIcon from "../../public/linkedin-icon.svg";
+import Link from "next/link";
+import Image from "next/image";
+
+const EmailSection = () => {
+  return (
+    <section className="grid md:grid-cols-2 my-12 py-24 gap-4">
+      <div>
+        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <p className="text-[#adb7be] mb-4 max-w-md">
+          I&apos;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
+        </p>
+        <div className="socials flex flex-row gap-2"></div>
+      </div>
+    </section>
+  );
+};
+
+export default EmailSection;
