1d704293cc0bf8f9e22d8cbea8d320957981b645
huang wei <1119765689hw@gmail.com>
2023-10-12 16:18:15 +0800

    finish the whole project

--- components/ProjectTag/index.tsx
+++ components/ProjectTag/index.tsx
@@ -6,11 +6,13 @@ interface ProjectTagProps {
   isSelected: boolean;
 }
 
-const ProjectTag = ({name, onClick, isSelected}: ProjectTagProps) => {
-  const buttonStyles = isSelected ? "border-purple-500 text-white" : "text-[#adb7be] border-slate-600 hover:border-white";
-  return <button onClick={onClick} className={`${buttonStyles} rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer`}>
-    {name}
-  </button>;
+const ProjectTag = ({ name, onClick, isSelected }: ProjectTagProps) => {
+  const buttonStyles = isSelected ? "border-primary-500 text-white" : "text-[#adb7be] border-slate-600 hover:border-white";
+  return (
+    <button onClick={onClick} className={`${buttonStyles} rounded-full border-2 border-primary-500 px-6 py-3 text-xl cursor-pointer`}>
+      {name}
+    </button>
+  );
 };
 
 export default ProjectTag;
--- components/TabButton/index.tsx
+++ components/TabButton/index.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 interface TabButtonProps {
-    active: boolean;
-    selectTab: () => void;
-    children: React.ReactNode;
+  active: boolean;
+  selectTab: () => void;
+  children: React.ReactNode;
 }
 
-const TabButton = ({ active, selectTab, children }: TabButtonProps ) => {
-  const buttonClasses = active ? "text-white border-b border-purple-500" : "text-[#adb7be]";
+const variants = {
+  default: {
+    width: "0%",
+  },
+  active: {
+    width: "calc(100% - 0.75rem)",
+  },
+};
+
+const TabButton = ({ active, selectTab, children }: TabButtonProps) => {
+  const buttonClasses = active ? "text-white" : "text-[#adb7be]";
   return (
     <button onClick={selectTab} className="">
       <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>{children}</p>
+      <motion.div variants={variants} animate={active ? "active" : "default"} className="h-1 bg-primary-500 mt-2 mr-3" />
     </button>
   );
 };
