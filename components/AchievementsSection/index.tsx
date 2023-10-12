53beaa43a35f5a1006a79c26a360da5fab5f7160
huang wei <1119765689hw@gmail.com>
2023-10-12 16:36:31 +0800

    improve some place

--- components/AchievementsSection/index.tsx
+++ components/AchievementsSection/index.tsx
@@ -24,7 +24,7 @@ const achievementsList = [
 const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-8">
-      <div className="border-[#33353f] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
+      <div className="border-[#33353f] border rounded-md py-8 px-16 flex flex-wrap sm:flex-row items-center sm:justify-between justify-center gap-3">
         {achievementsList.map((achievement, index) => {
           return (
             <div key={index} className="flex flex-col items-center justify-center mx-4">
